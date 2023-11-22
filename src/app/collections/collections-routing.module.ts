f8e745479695fc08f0af5f90ea7591a1aa28311b
Nina Hackenbroich <ninahackenbroich@MBP-von-Nina.fritz.box>
2023-11-22 18:00:29 +0100

    routing modules

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+const routes: Routes = [
+  { path: 'collections', component: CollectionsHomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
