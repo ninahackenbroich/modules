2f1fdb9b12aa67c402df4a9a5c8b74a65bd1c200
Nina Hackenbroich <ninahackenbroich@MBP-von-Nina.fritz.box>
2023-11-29 12:45:51 +0100

    reusable tabs component

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -5,12 +5,20 @@ import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
 import { TableComponent } from './table/table.component';
 import { SharedModule } from '../shared/shared.module';
+import { TabsComponent } from './tabs/tabs.component';
+import { BiographyComponent } from './biography/biography.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 
 
 @NgModule({
   declarations: [
     CollectionsHomeComponent,
-    TableComponent
+    TableComponent,
+    TabsComponent,
+    BiographyComponent,
+    CompaniesComponent,
+    PartnersComponent
   ],
   imports: [
     CommonModule,
